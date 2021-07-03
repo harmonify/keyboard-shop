248c98b63d4bbd7d7381022ba9d2a71d9e6c41d2
Wendy Surya Wijaya <wendy18.sw@gmail.com>
2021-07-03 15:47:06 +0700

    update exported sql file

--- db_finalproject.sql
+++ db_finalproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 01, 2021 at 12:06 PM
+-- Generation Time: Jul 03, 2021 at 10:45 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
@@ -42,7 +42,18 @@ CREATE TABLE `tb_users` (
 INSERT INTO `tb_users` (`id`, `username`, `userpass`, `userimg`, `userrole`) VALUES
 (1, 'admin', '$2y$10$w/RhD/jDR0H7Y3cE07e5CubJe0JNBw9slKh9OCLLbsLrGevcZwCB.', 'default_user_img.png', 'administrator'),
 (2, 'wendy', '$2y$10$FbLIolIbJffgKPOmKZUKpuxcFHb5AV2TL3WlGOaNPZWp9vPf6H0O.', 'keyboard10.jpg', 'user'),
-(46, 'admin2', '$2y$10$z8fUE/gIO00FyEeTbnx9AOzZr25hQr6JsO88xDWKOtW5NkSYnwRiu', 'keyboard1.jpg', 'administrator');
+(3, 'admin2', '$2y$10$z8fUE/gIO00FyEeTbnx9AOzZr25hQr6JsO88xDWKOtW5NkSYnwRiu', 'keyboard1.jpg', 'administrator'),
+(4, 'ryan', '$2y$10$FnWgCFsd.QNl.EnB6UAWbuCgtV2FVAPIc.Gr.ONsPvvB3wEOGfwhe', 'keyboard11.jpg', 'user'),
+(5, 'bian', '$2y$10$itZD1GUrf7dW9RCIzDgKAe023HcXl6/lJJ/W2qDMYAsXov/IuEyMG', 'default_user_img.png', 'user'),
+(6, 'asdf', '$2y$10$e5Ax1rAsOVyt4ygvvbtrnOJg1eY2ii.6WbFP0TKH61k2KbJSCGKMy', 'default_user_img.png', 'administrator'),
+(7, 'jekyll', '$2y$10$cVCS8chBK6nge0qxso8zs.Y5VX/lm2zdSqh6pUHN7k3A1ee9rECP2', 'keyboard4.jpg', 'user'),
+(8, 'deyy', '$2y$10$JBrQRWEtnOOMKTwvlzpyueHR8XmaC.mpZgF9.VcC9Dw9XYrsGS19G', 'keyboard6.jpg', 'administrator'),
+(9, 'eyy', '$2y$10$S/9T4W3zPf.snoa8I51aU.FJX.VhsoDm77xATdKrYBZQW07pDL9tm', 'keyboard7.jpg', 'user'),
+(10, 'fff', '$2y$10$ih3LMjBXKg2SmTm4pf7baOYg.yL0bKzHdMRe7oUFM4W7IuTnOha0G', 'keyboard10.jpg', 'user'),
+(11, 'gell', '$2y$10$bwuFIwYfcpwonmTPgOJnyeRdu4tI3Vd0o/qMWtT2LeUybmuS0dnEO', 'keyboard2.jpg', 'administrator'),
+(12, 'harley', '$2y$10$cejsh/ifCqCjTIKiJbmLg.zaJ7wMDshUzF445O8K79RHP4X1zSDgW', 'keyboard9.jpg', 'user'),
+(13, 'ian', '$2y$10$QPkAhlVv9O.lDw64qe97Rua3im99.NxDvqIPkLX2kyBg.HHdJCGeO', 'keyboard8.jpg', 'user'),
+(14, 'jean', '$2y$10$5pwBMzf0sg2VRyFu3Sdo.OcFlWCrIoQR94tut.g8gcQ.F4lSCe.qO', 'keyboard5.jpg', 'user');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +73,7 @@ ALTER TABLE `tb_users`
 -- AUTO_INCREMENT for table `tb_users`
 --
 ALTER TABLE `tb_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
